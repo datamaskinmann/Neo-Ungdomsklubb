435da911e243242cc7045cce483b5d241727fd4f
datamaskinmann <svens@uia.no>
2021-11-08 03:33:34 +0100

    Sql script

---
+++ createdb.sql
@@ -0,0 +1,62 @@
+create database if not exists neo;
+
+use neo;
+
+create table if not exists adress (
+	id int auto_increment unique not null,
+	streetName varchar(255) not null,
+	streetNumber int not null,
+	postalCode int not null,
+	city varchar(255) not null
+);
+
+create table if not exists member (
+	id int auto_increment primary key unique not null,
+	firstname varchar(255) not null,
+	lastname varchar(255) not null,
+	phonenumber varchar(255) not null,
+	email varchar(255) not null,
+	password varchar(255) not null,
+	gender enum('male', 'female'),
+	adressId int not null,
+	dateCreated datetime default(now()),
+	foreign key(adressId) references adress(id)
+);
+
+create table if not exists pastMember (
+	memberId int primary key unique not null,
+	foreign key(memberId) references member(id)
+);
+
+create table if not exists interest(
+	id int auto_increment primary key unique not null,
+	tag varchar(16) not null,
+	description varchar(255) not null
+);
+
+create table if not exists memberInterest(
+	memberId int unique not null,
+	interestId int unique not null,
+	foreign key(memberId) references member(id),
+	foreign key(interestId) references interest(id),
+    primary key(memberId, interestId)
+);
+
+create table if not exists activity(
+	id int primary key unique not null auto_increment,
+	tag varchar(16) not null,
+	description varchar(255) not null
+);
+
+create table if not exists activityType(
+	activityId int not null primary key unique not null,
+	type varchar(16) not null unique
+);
+
+create table if not exists activityParticipant(
+	memberId int not null unique,
+	activityId int not null unique,
+	foreign key(memberId) references member(id),
+	foreign key(activityId) references activity(id),
+    primary key(memberId, activityId)
+);
